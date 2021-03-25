5b51dea476a31b880afcd86524de04be5b7d0a04
Sifou Biri <sifeddinebiri@gmail.com>
2021-03-24 20:36:05 -0700

    custom

---
+++ custom.el
@@ -0,0 +1,137 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   (vector "#2d2d2d" "#f2777a" "#99cc99" "#ffcc66" "#6699cc" "#cc99cc" "#66cccc" "#cccccc"))
+ '(beacon-color "#f2777a")
+ '(custom-enabled-themes '(modus-vivendi))
+ '(evil-emacs-state-cursor '("#E57373" hbar))
+ '(evil-insert-state-cursor '("#E57373" bar))
+ '(evil-normal-state-cursor '("#FFEE58" box))
+ '(evil-visual-state-cursor '("#C5E1A5" box))
+ '(fci-rule-color "#515151" t)
+ '(flycheck-color-mode-line-face-to-color 'mode-line-buffer-id)
+ '(flymake-error-bitmap '(flymake-double-exclamation-mark modus-theme-fringe-red))
+ '(flymake-note-bitmap '(exclamation-mark modus-theme-fringe-cyan))
+ '(flymake-warning-bitmap '(exclamation-mark modus-theme-fringe-yellow))
+ '(frame-background-mode nil)
+ '(frame-brackground-mode 'dark)
+ '(fringe-mode '(1 . 1) nil (fringe))
+ '(global-whitespace-cleanup-mode t)
+ '(highlight-indent-guides-auto-enabled nil)
+ '(highlight-symbol-colors
+   '("#FFEE58" "#C5E1A5" "#80DEEA" "#64B5F6" "#E1BEE7" "#FFCC80"))
+ '(highlight-symbol-foreground-color "#E0E0E0")
+ '(highlight-tail-colors '(("#ed0547ad8099" . 0) ("#424242" . 100)))
+ '(hl-todo-keyword-faces
+   '(("HOLD" . "#cfdf30")
+     ("TODO" . "#feacd0")
+     ("NEXT" . "#b6a0ff")
+     ("THEM" . "#f78fe7")
+     ("PROG" . "#00d3d0")
+     ("OKAY" . "#4ae8fc")
+     ("DONT" . "#80d200")
+     ("FAIL" . "#ff8059")
+     ("BUG" . "#ff8059")
+     ("DONE" . "#44bc44")
+     ("NOTE" . "#f0ce43")
+     ("KLUDGE" . "#eecc00")
+     ("HACK" . "#eecc00")
+     ("TEMP" . "#ffcccc")
+     ("FIXME" . "#ff9977")
+     ("XXX+" . "#f4923b")
+     ("REVIEW" . "#6ae4b9")
+     ("DEPRECATED" . "#bfd9ff")))
+ '(ibuffer-deletion-face 'modus-theme-mark-del)
+ '(ibuffer-filter-group-name-face 'modus-theme-mark-symbol)
+ '(ibuffer-marked-face 'modus-theme-mark-sel)
+ '(ibuffer-title-face 'modus-theme-header)
+ '(jdee-db-active-breakpoint-face-colors (cons "#1B2229" "#D08770"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#1B2229" "#A3BE8C"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#1B2229" "#4f5b66"))
+ '(js2-language-version 200)
+ '(line-spacing 0.24)
+ '(lsp-ui-imenu-colors '("#7FC1CA" "#A8CE93"))
+ '(objed-cursor-color "#BF616A")
+ '(package-selected-packages
+   '(envrc uptimes shfmt dotenv-mode daemons osx-location htmlize lua-mode gnuplot sudo-edit flycheck-ledger ledger-mode dash-at-point origami regex-tool info-colors flycheck-clojure cider elein cljsbuild-mode clojure-mode slime-company slime cask-mode flycheck-relint cl-libify flycheck-package highlight-quoted macrostep cl-lib-highlight aggressive-indent immortal-scratch auto-compile ipretty elisp-slime-nav paredit nginx-mode company-nixos-options nixos-options nix-buffer nix-sandbox nixpkgs-fmt nix-mode company-terraform terraform-mode docker-compose-mode dockerfile-mode docker yaml-mode flycheck-rust racer rust-mode flycheck-nim nim-mode j-mode merlin-eldoc merlin tuareg sqlformat projectile-rails yard-mode bundler yari robe ruby-compilation inf-ruby rspec-mode ruby-hash-syntax psci psc-ide purescript-mode flycheck-elm elm-test-runner elm-mode dhall-mode dante haskell-mode reformatter toml-mode company-anaconda anaconda-mode pip-requirements restclient httprepl haml-mode css-eldoc skewer-less sass-mode rainbow-mode tagedit org-pomodoro writeroom-mode org-cliplink grab-mac-link company-php smarty-mode php-mode add-node-modules-path skewer-mode js-comint coffee-mode xref-js2 prettier-js typescript-mode js2-mode json-mode erlang csv-mode markdown-mode textile-mode crontab-mode alert ibuffer-projectile github-review forge github-clone bug-reference-github yagist git-commit magit-todos magit git-timemachine gitconfig-mode gitignore-mode git-blamed vc-darcs browse-at-remote whitespace-cleanup-mode which-key highlight-escape-sequences whole-line-or-region move-dup page-break-lines multiple-cursors avy browse-kill-ring symbol-overlay goto-line-preview beacon mode-line-bell vlf list-unicode-display unfill mmm-mode session windswap switch-window company-quickhelp company marginalia consult-flycheck embark-consult projectile consult embark selectrum-prescient selectrum flycheck-color-mode-line flycheck ibuffer-vc rg wgrep-ag ag wgrep anzu diff-hl diredfl disable-mouse default-text-scale ns-auto-titlebar dimmer color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized command-log-mode scratch diminish exec-path-from-shell gnu-elpa-keyring-update fullframe seq yaml-imenu repl-toggle ts-comint company-quickhelp-terminal flycheck-pos-tip tide nova-theme nyan-mode js2-highlight-vars modus-vivendi-theme lush-theme gruber-darker-theme nimbus-theme mlso-theme klere-theme grandshell-theme fantom-theme paren-face clojure-mode-extra-font-locking rainbow-delimiters swift-mode))
+ '(pdf-view-midnight-colors (cons "#c0c5ce" "#2b303b"))
+ '(pos-tip-background-color "#3a933a933a93")
+ '(pos-tip-foreground-color "#9E9E9E")
+ '(rtog/interactivep t)
+ '(rtog/mode-repl-alist '((js2-mode . nodejs-repl) (typescript-mode . run-ts)))
+ '(rustic-ansi-faces
+   ["#2b303b" "#BF616A" "#A3BE8C" "#ECBE7B" "#8FA1B3" "#c678dd" "#46D9FF" "#c0c5ce"])
+ '(safe-local-variable-values
+   '((cider-shadow-default-options . "main")
+     (cider-default-cljs-repl . shadow)))
+ '(session-use-package t nil (session))
+ '(show-paren-mode t)
+ '(show-trailing-whitespace nil)
+ '(tabbar-background-color "#357535753575")
+ '(vc-annotate-background nil)
+ '(vc-annotate-background-mode nil)
+ '(vc-annotate-color-map
+   '((20 . "#f2777a")
+     (40 . "#f99157")
+     (60 . "#ffcc66")
+     (80 . "#99cc99")
+     (100 . "#66cccc")
+     (120 . "#6699cc")
+     (140 . "#cc99cc")
+     (160 . "#f2777a")
+     (180 . "#f99157")
+     (200 . "#ffcc66")
+     (220 . "#99cc99")
+     (240 . "#66cccc")
+     (260 . "#6699cc")
+     (280 . "#cc99cc")
+     (300 . "#f2777a")
+     (320 . "#f99157")
+     (340 . "#ffcc66")
+     (360 . "#99cc99")))
+ '(vc-annotate-very-old-color nil)
+ '(window-divider-mode nil)
+ '(xterm-color-names
+   ["#000000" "#ff8059" "#44bc44" "#eecc00" "#29aeff" "#feacd0" "#00d3d0" "#a8a8a8"])
+ '(xterm-color-names-bright
+   ["#181a20" "#f4923b" "#80d200" "#cfdf30" "#72a4ff" "#f78fe7" "#4ae8fc" "#ffffff"]))
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  ;; '(default ((t (:inherit nil :extend nil :stipple nil :background "#292a2f" :Foreground "#cccccc" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight medium :height 130 :width medium :foundry "nil" :family "SF Mono"))))
+;;  '(cursor ((t (:background "red"))))
+;;  '
+;;  '(fringe ((t (:background "#292a2f" :foreground "#999999"))))
+;;  '(line-number ((t (:background "#292a2f" :foreground "#999999"))))
+;;  '(line-number-current-line ((t (:inherit line-number :foreground "white" :weight bold))))
+;;  '(mode-line ((t (:background "#515151" :foreground "#cccccc" :box (:line-width 1 :color "#515151") :weight normal))))
+;;  '(symbol-overlay-default-face ((t (:inherit highlight :underline nil :weight medium)))))
+
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight medium :height 110 :width medium :background "#1F1F24" :foundry "nil" :family "SF Mono"))))
+ '(font-lock-string-face ((t (:foreground "#fd8273"))))
+ '(fringe ((t (:background "##1a1a1a" :foreground "#999999"))))
+ '(line-number ((t (:background "#1F1F24" :foreground "#999999"))))
+ '(line-number-current-line ((t (:inherit line-number :foreground "white" :weight bold))))
+ '(symbol-overlay-default-face ((t (:inherit highlight :underline nil :weight medium)))))
+
+(set-background-color "#1F1F24")
+
+                                        ;(set-background-color "#18171B")
+
+;; (set-foreground-color "#FFFFFF")
+
+;;
+;;
